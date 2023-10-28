c1fea0321e4eccbaf4fe8772c0ccc163cd933161
FIRST_NAME LAST_NAME <EMAIL_ADDRESS>
2023-10-28 14:47:58 +0200

    Eerste aanpassingen

--- Ofenen rekenen/Ofenen rekenen/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ofenen_rekenen.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
